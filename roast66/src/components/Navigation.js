fd294b9d750a44cd7a7518349a60cfd47d51b2ae
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-14 22:24:26 -0800

    cleanup

--- roast66/src/components/Navigation.js
+++ roast66/src/components/Navigation.js
@@ -1,6 +1,8 @@
 // src/components/layout/Navigation.jsx
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaInstagram } from "react-icons/fa";
+
 import logo from "../logo.png"; // Adjust the path if necessary
 
 function Navigation() {
@@ -9,33 +11,62 @@ function Navigation() {
       <div className="bg-secondary container mx-auto flex items-center justify-between p-4 rounded">
         {/* Brand Logo */}
         <div className="text-2xl font-bold">
-          <NavLink to="/" title="Roast 66 Coffee Home" className="hover:text-accent flex items-center">
+          <NavLink
+            to="/"
+            title="Roast 66 Coffee Home"
+            className="hover:text-accent flex items-center"
+          >
             <img src={logo} alt="Roast 66" className="h-8 inline-block mr-2" />
             <span className="text-dark">Roast 66 Coffee</span>
           </NavLink>
         </div>
 
         {/* Navigation Links */}
-        <ul className="flex space-x-6">
+        <ul className="flex space-x-6 items-center">
           <li>
-            <NavLink to="/" className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent">
+            <NavLink
+              to="/"
+              className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent"
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/menu" className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent">
+            <NavLink
+              to="/menu"
+              className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent"
+            >
               Menu
             </NavLink>
           </li>
           <li>
-            <NavLink to="/order" className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent">
+            <NavLink
+              to="/order"
+              className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent"
+            >
               Order
             </NavLink>
           </li>
+
           <li>
-            <NavLink to="/admin" className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent">
+            <NavLink
+              to="/admin"
+              className="text-dark hover:text-accent no-underline border-b-2 border-transparent hover:border-accent"
+            >
               Admin
             </NavLink>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/roast66coffee"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-dark hover:text-accent no-underline flex items-center border-b-2 border-transparent hover:border-accent"
+              title="Follow us on Instagram"
+            >
+              <FaInstagram className="text-2xl mr-1" />
+              Instagram
+            </a>
           </li>
         </ul>
       </div>
