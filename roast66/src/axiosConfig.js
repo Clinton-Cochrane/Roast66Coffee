26a7e48f11ef783085c484bbc69c2e4f7eb0ca90
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-15 00:50:56 -0800

    troubleshooting

--- roast66/src/axiosConfig.js
+++ roast66/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'https://roast66coffee.onrender.com/api',
 });
 
 export default instance;
