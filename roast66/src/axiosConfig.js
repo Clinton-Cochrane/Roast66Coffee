c8e32e57553340216984f854360a9424acbbfdb8
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-14 23:56:54 -0800

    update the connection string

--- roast66/src/axiosConfig.js
+++ roast66/src/axiosConfig.js
@@ -2,7 +2,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : 'http://localhost:5001/api'
-});
+    baseURL : process.env.REACT_APP_API_URL});
 
 export default instance;
