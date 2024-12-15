9d075a803d96b3fd993bf4eea7ffbd1032688fec
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-14 16:28:51 -0800

    update roast 66 to roast 66 coffee in outboard text

--- roast66/src/components/Customer/About.js
+++ roast66/src/components/Customer/About.js
@@ -7,10 +7,10 @@ function About() {
     <div >
       <Card className="min-h-40" title="About Us">
         <p className="mb-4">
-          Roast 66 is a unique coffee shop on wheels, bringing your favorite coffee directly to you! Whether you are at home, at work, or on the go, we can deliver freshly brewed coffee to your location, given enough time and depending on the drink.
+          Roast 66 Coffee is a unique coffee shop on wheels, bringing your favorite coffee directly to you! Whether you are at home, at work, or on the go, we can deliver freshly brewed coffee to your location, given enough time and depending on the drink.
         </p>
         <p>
-          Our owner has always had a passion for cars and coffee. Combining these two loves, she created Roast 66 as a dream come true. Our coffee trailer roams the streets, serving up delicious beverages and bringing joy to coffee lovers everywhere. Come and experience the magic of cars and coffee with us!
+          Our owner has always had a passion for cars and coffee. Combining these two loves, she created Roast 66 Coffee as a dream come true. Our coffee trailer roams the streets, serving up delicious beverages and bringing joy to coffee lovers everywhere. Come and experience the magic of cars and coffee with us!
         </p>
       </Card>
     </div>
--- roast66/src/components/Customer/Location.js
+++ roast66/src/components/Customer/Location.js
@@ -6,7 +6,7 @@ function Location() {
   return (
     <div>
       <Card className="min-h-40" title="Our Location">
-      <p>Roast 66 is always on the move in our coffee trailer! We travel around the city, bringing our delightful coffee to various locations. Follow us on social media to find out where we will be next, or contact us to request a visit to your area. We cannot wait to serve you a delicious cup of coffee, wherever you are!</p>
+      <p>Roast 66 Coffee is always on the move in our coffee trailer! We travel around the area, bringing our delightful coffee to various locations. Follow us on social media to find out where we will be next, or contact us to request a visit to your area. We cannot wait to serve you a delicious cup of coffee, wherever you are!</p>
       </Card>
       
     </div>
