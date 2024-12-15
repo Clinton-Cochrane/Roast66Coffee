28de86476b86ad3e49e9b9898190beb36e313ee4
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-14 16:34:38 -0800

    missed one for the coffee string

--- roast66/src/components/Customer/Welcome.js
+++ roast66/src/components/Customer/Welcome.js
@@ -7,7 +7,7 @@ import logo from "../../logo.png"; // Adjust path if necessary
 function Welcome() {
   return (
     <div className=" bg-gray-100 flex items-center justify-center">
-      <Card title="Welcome to Roast 66">
+      <Card title="Welcome to Roast 66 Coffee">
         <img src={logo} alt="Roast 66 Coffee Logo" className='w-64 mx-auto my-4'/>
         <p className="text-lg text-gray-700">Your favorite coffee shop</p>
       </Card>
