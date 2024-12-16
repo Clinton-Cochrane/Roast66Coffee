b37f7363887e16bd70bd00688bf40acc65dcce96
Clint Cochrane <Clinton.Edward.Cochrane@gmail.com>
2024-12-15 17:01:58 -0800

    Track migrations and update .gitignore

---
+++ CoffeeShopApi/Migrations/20240615000635_UpdateOrdersToLowercase.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShopApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrdersToLowercase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_menuitems_MenuItemId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "orders");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderDate",
+                table: "orders",
+                newName: "orderdate");
+
+            migrationBuilder.RenameColumn(
+                name: "MenuItemId",
+                table: "orders",
+                newName: "menuitemid");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerName",
+                table: "orders",
+                newName: "customername");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "orders",
+                newName: "id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_MenuItemId",
+                table: "orders",
+                newName: "IX_orders_menuitemid");
+
+            migrationBuilder.AddColumn<int>(
+                name: "quantity",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_orders",
+                table: "orders",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_menuitems_menuitemid",
+                table: "orders",
+                column: "menuitemid",
+                principalTable: "menuitems",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_menuitems_menuitemid",
+                table: "orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_orders",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "quantity",
+                table: "orders");
+
+            migrationBuilder.RenameTable(
+                name: "orders",
+                newName: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "orderdate",
+                table: "Orders",
+                newName: "OrderDate");
+
+            migrationBuilder.RenameColumn(
+                name: "menuitemid",
+                table: "Orders",
+                newName: "MenuItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "customername",
+                table: "Orders",
+                newName: "CustomerName");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Orders",
+                newName: "Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_orders_menuitemid",
+                table: "Orders",
+                newName: "IX_Orders_MenuItemId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_menuitems_MenuItemId",
+                table: "Orders",
+                column: "MenuItemId",
+                principalTable: "menuitems",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ CoffeeShopApi/Migrations/20240618213130_AddNotificationSettings.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CoffeeShopApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotificationSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_menuitems_menuitemid",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_orders_menuitemid",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "menuitemid",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "quantity",
+                table: "orders");
+
+            migrationBuilder.CreateTable(
+                name: "notificationsettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    phonenumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notificationsettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderitems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    orderid = table.Column<int>(type: "integer", nullable: false),
+                    menuitemid = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderitems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orderitems_menuitems_menuitemid",
+                        column: x => x.menuitemid,
+                        principalTable: "menuitems",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orderitems_orders_orderid",
+                        column: x => x.orderid,
+                        principalTable: "orders",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderitems_menuitemid",
+                table: "orderitems",
+                column: "menuitemid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderitems_orderid",
+                table: "orderitems",
+                column: "orderid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notificationsettings");
+
+            migrationBuilder.DropTable(
+                name: "orderitems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "menuitemid",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "quantity",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_menuitemid",
+                table: "orders",
+                column: "menuitemid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_menuitems_menuitemid",
+                table: "orders",
+                column: "menuitemid",
+                principalTable: "menuitems",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
