7cc3f3341488c6f1dd388afc19df6bcb4c4fb4b8
Clinton-Cochrane <clinton.edward.cochrane@gmail.com>
2026-03-27 17:25:40 -0700

    fix for ci testing

--- CoffeeShopApi.Tests/Integration/WebAppFactory.cs
+++ CoffeeShopApi.Tests/Integration/WebAppFactory.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 namespace CoffeeShopApi.Tests.Integration;
 
@@ -11,5 +13,13 @@ public class WebAppFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+        // Ensure CORS startup succeeds even if the host process sets a non-Testing environment.
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["AllowedOrigins"] = "http://localhost"
+            });
+        });
     }
 }
